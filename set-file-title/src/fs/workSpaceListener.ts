5d0fea33a215a26a2a24c35826147ff90aadd18e
bugvvd <bugvvd@163.com>
2022-05-22 00:13:07 +0800

    correctlly set up the project. correctly set extension activation as 'onStartupFinished', trouble with understanding Disposable. Can't figure out the warnings: (1) rejected promise not handled within 1 second: Error: ENOENT: no such file or directory, watch '/testDir/HEAD' (2)/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/api/node/extensionHostProcess.js:102 stack trace: Error: ENOENT: no such file or directory, watch '/testDir/HEAD'

---
+++ set-file-title/src/fs/workSpaceListener.ts
@@ -0,0 +1,6 @@
+import * as vscode from "vscode";
+
+export function workSpaceListener() {
+//   let watcher = vscode.workspace.createFileSystemWatcher("**/*.ts");
+//   watcher.onDidCreate((uri) => console.log(uri));
+}
